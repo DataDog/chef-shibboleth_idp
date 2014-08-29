9760293c02f18a3ae9eaccca3801b50a25a9f0e8
Bartek Jarocki <bartek@smatly.com>
2014-08-29 13:34:28 +0200

    Add name to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer_email  "elliot.kendall@ucsf.edu"
 license           "Apache 2.0"
 description       "Installs Shibboleth IdP"
 version           "0.1.2"
+name              "shibboleth_idp"
 
 recipe "shibboleth_idp", "Installs Shibboleth IdP"
 
